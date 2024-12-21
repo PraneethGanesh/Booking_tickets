2810a5611bfd0ccdfdda18f085138c524388b222
PraneethGanesh <140528564+PraneethGanesh@users.noreply.github.com>
2024-12-21 17:05:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Praneeth@03',
+  password: '*********',
   database: 'movie_booking'
 });
 
